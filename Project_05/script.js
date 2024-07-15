14ed7ec7b755e8fbaf125d19c720302390594919
Mauryaavikas <mauryaavikas@gmail.com>
2024-07-15 15:59:01 +0530

    js add

---
+++ Project_05/script.js
@@ -0,0 +1,32 @@
+const inputBox = document.getElementById('inputBox')
+const addBtn = document.getElementById('addBtn')
+const todoList = document.getElementById('todoList')
+
+const addtodo = () => {
+  const inputText = inputBox.value.trim()
+  if(inputText.length <= 0){
+    alert("Please enter some task")
+    return false
+
+  }
+  const li = document.createElement('li')
+  const p = document.createElement('p')
+ 
+  p.innerHTML = inputText
+  li.appendChild(p)
+
+ const deleteBtn = document.createElement("button")  
+  deleteBtn.innerText = "Remove"
+  li.appendChild(deleteBtn)
+
+  todoList.appendChild(li)
+  inputBox.value = ""
+}
+const removeTodo = (e) =>{
+  if(e.target.innerHTML === "Remove"){
+    todoList.removeChild(e.target.parentElement)
+  }
+ 
+}
+addBtn.addEventListener('click', addtodo)
+todoList.addEventListener('click',removeTodo)
